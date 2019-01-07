88a5826bdb505064386b3933ec615a38ce13ed05
Raymundo Armendariz <raymundo.armendariz@gmail.com>
2019-01-07 16:37:22 -0700

    adding Game Engine Exception tests

---
+++ src/test/java/com/bol/game/util/GameEngine_ExceptionsTests.java
@@ -0,0 +1,65 @@
+package com.bol.game.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bol.game.domain.GameState;
+import com.bol.game.domain.Player;
+import com.bol.game.util.exception.GameStateException;
+import com.bol.game.util.exception.InvalidPlayerIdException;
+import com.bol.game.util.exception.PlayerAlreadyActiveException;
+
+public class GameEngine_ExceptionsTests {
+
+	public GameEngine gameEngine;
+
+	@Before
+	public void init() {
+		gameEngine = new GameEngine();
+	}
+
+	@Test
+	public void registerPlayerWithInvalidID_throwsInvalidPlayerIdException() {
+		try {
+			gameEngine.registerPlayer("Player3");
+		} catch (Exception ex) {
+			assertThat(ex).isInstanceOf(InvalidPlayerIdException.class);
+			assertThat(ex.getMessage()).isEqualTo("Invalid Player Id");
+		}
+	}
+	
+	@Test
+	public void registerPlayer1WhenAlreadyActive_throwsInvalidPlayerIdException() {
+		try {
+			gameEngine.getGame().setPlayer1Active(true);
+			gameEngine.registerPlayer(Player.PLAYER_1.toString());
+		} catch (Exception ex) {
+			assertThat(ex).isInstanceOf(PlayerAlreadyActiveException.class);
+			assertThat(ex.getMessage()).isEqualTo("Player 1 is already Active");
+		}
+	}
+	
+	@Test
+	public void registerPlayer2WhenAlreadyActive_throwsInvalidPlayerIdException() {
+		try {
+			gameEngine.getGame().setPlayer2Active(true);
+			gameEngine.registerPlayer(Player.PLAYER_2.toString());
+		} catch (Exception ex) {
+			assertThat(ex).isInstanceOf(PlayerAlreadyActiveException.class);
+			assertThat(ex.getMessage()).isEqualTo("Player 2 is already Active");
+		}
+	}
+	
+	@Test
+	public void registerPlayerOnStateStarted_throwsGameStateException() {
+		try {
+			gameEngine.getGame().setState(GameState.STARTED);
+			gameEngine.registerPlayer(Player.PLAYER_2.toString());
+		} catch (Exception ex) {
+			assertThat(ex).isInstanceOf(GameStateException.class);
+			assertThat(ex.getMessage()).isEqualTo("The current Game state: STARTED does not allow registration");
+		}
+	}
+}
