1a653e9e3f7be9bfa78f22cc50e7b848755a506a
Raymundo Armendariz <raymundo.armendariz@gmail.com>
2019-01-17 17:46:37 -0700

    adding test to sse subscription

--- src/test/java/com/bol/game/controller/GameControllerTests.java
+++ src/test/java/com/bol/game/controller/GameControllerTests.java
@@ -1,24 +1,28 @@
 package com.bol.game.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
+import javax.servlet.AsyncListener;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockAsyncContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.test.web.servlet.MvcResult;
 
-import com.bol.game.domain.Game;
-import com.bol.game.domain.Turn;
 import com.bol.game.service.GameService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -28,13 +32,13 @@ public class GameControllerTests {
 
 	@Autowired
 	private MockMvc mockMvc;
-
+	
 	@MockBean
 	private GameService gameService;
-	
+
 	@Before
 	public void setUp() {
-		 JacksonTester.initFields(this, new ObjectMapper());
+		JacksonTester.initFields(this, new ObjectMapper());
 	}
 
 	@Test
@@ -52,4 +56,15 @@ public class GameControllerTests {
 		this.mockMvc.perform(post("/playTurn/PLAYER_2/14")).andExpect(status().isUnprocessableEntity());
 	}
 	
+	@Test
+	public void testSubscribeToSSE_StatusIsOK() throws Exception {
+		MvcResult mvcResult = this.mockMvc.perform(get("/sse")).andExpect(request().asyncStarted()).andReturn();
+		MockAsyncContext ctx = (MockAsyncContext) mvcResult.getRequest().getAsyncContext();
+		for (AsyncListener listener : ctx.getListeners()) {
+			listener.onTimeout(null);
+		}
+		this.mockMvc.perform(asyncDispatch(mvcResult)).andExpect(status().isOk())
+				.andExpect(content().contentType("text/event-stream;charset=UTF-8"));
+	}
+
 }
